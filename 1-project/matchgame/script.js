0caca1e5e7623392cf7ab97c5c5170f1b618a7af
rohai <rohai.sul@gmail.com>
2020-01-08 20:44:02 +0800

    added timer onbody click

--- 1-project/matchgame/script.js
+++ 1-project/matchgame/script.js
@@ -1,64 +1,45 @@
 console.log("game!")
 
-/*----- constants -----*/
 
-
-// var btn = document.querySelectorAll('.card');
-// // console.log(btn);
-// for (var i = 0; i < btn.length; i++){
-// var btns = btn[i];
-// // console.log(btns);
-// btns.addEventListener('click', function(){
-
-// var cardFrontArray = document.querySelectorAll('#front');
-
-
-// for (var j = 0; j < cardFrontArray.length; j++) {
-//     // console.log(cardFrontArray[j]);
-//     var backCard = document.querySelectorAll('#back');
-
-//     // document.querySelector('#front').style.display = 'none';
-//     // document.querySelector('#back').style.display = 'block';
-//     cardFrontArray[j].style.display = 'none';
-//     backCard[j].style.display = 'block';
-//     // console.log(backCard.length);
-// var autoShowFront = setTimeout(function() {
-//     document.querySelector('#front').style.display = 'block';
-//     document.querySelector('#back').style.display = 'none';;
-// }, 2000);
-// }
-// // console.log(cardFrontArray);
-
-// })
-// }
-
-// var displayCard = function(){
-//     card.classList.toggle('do-flip');
-
-// }
-
-
-var card = document.querySelectorAll('.cardz');
+// Game timer
+var second = 0, minute = 0, hour = 0;
+var timer = document.querySelector('.timer');
+var interval;
+var card = document.querySelectorAll('.cardz');//selects element
 console.log(card)
 
+
+
+
+
+
+
+
 card.forEach(function(item){
-    item.addEventListener('click', function(){
+         item.addEventListener('click', function(){ //adds eventlistener to element on click
         console.log('yay')
-        // var i = 0;
-        // var flipCard = document.querySelector('.flip-card');
-        // var flipCardArr = flipCard[i];
-        // console.log( flipCardArr);
-        // var i = 0;
-        // var cards = card[i];
-        // console.log(card[i]);
-        // card.forEach(function(flip){
-        item.classList.toggle('is-flipped');
+        item.classList.toggle('is-flipped');//flips element
 
-
-        var autoShow = setTimeout(function() {
+        var autoShow = setTimeout(function() {//auto undo flip in 2secs
             item.classList.toggle('is-flipped');
         }, 2000);
     })
+
 })
 
+function startTimer() {
+        interval = setInterval(function() {
+        timer.innerHTML = minute + 'mins ' + second + 'secs';//adds text into div
+        second++;
+        if (second == 60) {
+          minute++;
+          second = 0;
+        }
+        if (minute == 60) {
+          hour++;
+          minute = 0;
+        }
+      }, 1000);
+}
 
+document.body.addEventListener("click", startTimer);
