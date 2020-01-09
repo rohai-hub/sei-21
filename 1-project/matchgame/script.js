e83f6a05b246fbb503b5e36985ea78610533a66f
rohai <rohai.sul@gmail.com>
2020-01-09 10:05:18 +0800

    image Array pushed into cards

--- 1-project/matchgame/script.js
+++ 1-project/matchgame/script.js
@@ -5,13 +5,58 @@ console.log("game!")
 var second = 0, minute = 0, hour = 0;
 var timer = document.querySelector('.timer');
 var interval;
+
+//flipping
 var card = document.querySelectorAll('.cardz');//selects element
 console.log(card)
 
+//restart game
+var reset = document.querySelector('#reset-button');
 
 
+var cardsOpen = [];
+
+var cardsArray = [
+    { name: "cat", src:"images/cat.jpg", alt:"cat"},
+    { name: "bat", src:"images/bat.jpg", alt:"bat"},
+    { name: "rat", src:"images/rat.jpg", alt:"rat"},
+    { name: "cat", src:"images/cat.jpg", alt:"cat"},
+    { name: "rat", src:"images/rat.jpg", alt:"rat"},
+    { name: "bat", src:"images/bat.jpg", alt:"bat"},
+    { name: "rat", src:"images/rat.jpg", alt:"rat"},
+    { name: "cat", src:"images/cat.jpg", alt:"cat"},
+    { name: "bat", src:"images/bat.jpg", alt:"bat"}
+];
+var myCards = document.getElementsByClassName('card__face--back');
+var i = 0; // index for cardsArray
+var j = 0; //index for div array to place images
 
 
+//------------------functions---------------------//
+
+function startGame(){
+        cards = shuffle(cardsArray);
+        console.log(cards);
+
+        for( var i = 0; i < cardsArray.length; i++ ){
+
+            myCards.innerHTML = '';                     //removes existing cards
+
+            var img = document.createElement("img");
+            var cardImg = cardsArray[i];
+
+            img.src = cardImg.src;
+            console.log(cardImg.src);
+            img.alt = cardImg.alt;
+
+
+            myCards[j].appendChild(img);
+            j++;
+            };
+
+        startTimer();
+
+}
 
 
 
@@ -22,7 +67,7 @@ card.forEach(function(item){
 
         var autoShow = setTimeout(function() {//auto undo flip in 2secs
             item.classList.toggle('is-flipped');
-        }, 2000);
+        }, 3000);
     })
 
 })
@@ -41,5 +86,33 @@ function startTimer() {
         }
       }, 1000);
 }
+document.body.onload = startGame();
 
-document.body.addEventListener("click", startTimer);
+
+function resetGame(){
+       // Resets timer
+        var second = 0;
+        var minute = 0;
+        var hour = 0;
+        var timer = document.querySelector('.timer');
+        timer.innerHTML = minute + 'mins ' + second + 'secs';
+
+}
+console.log(interval);
+reset.addEventListener("click", resetGame);
+
+
+//shuffles cardsArray
+function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+        }
+
+  return array;
+}
